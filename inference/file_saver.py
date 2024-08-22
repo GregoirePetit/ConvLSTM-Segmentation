0cc4f9458e56d8efd240ef21d09ac1718816feb5
GregoirePetit <g.petit360@gmail.com>
2024-08-22 12:27:35 +0200

    file saver

--- inference/file_saver.py
+++ inference/file_saver.py
@@ -8,6 +8,15 @@ def save_segmented_frames_as_npy(segmented_frames, output_path):
     np.save(output_path, segmented_frames)
 
 
+def save_segmented_frames_as_npz(segmented_frames, output_path):
+    normalized_video = (
+        (segmented_frames - np.min(segmented_frames))
+        / (np.max(segmented_frames) - np.min(segmented_frames))
+        * 255
+    ).astype(np.uint8)
+    np.savez_compressed(output_path, normalized_video)
+
+
 def process_videos_in_folder(video_folder, model, output_folder):
     os.makedirs(output_folder, exist_ok=True)
     video_files = [f for f in os.listdir(video_folder) if f.endswith(".avi")]
@@ -21,4 +30,7 @@ def process_videos_in_folder(video_folder, model, output_folder):
         print(f"Processing {video_file}...")
         segmented_frames = segment_video(model, video_path)
         save_segmented_frames_as_npy(segmented_frames, output_path)
+        print(f"Saved segmented output to {output_path}")
+        output_path = os.path.splitext(output_path)[0] + ".npz"
+        save_segmented_frames_as_npz(segmented_frames, output_path)
         print(f"Saved segmented output to {output_path}")
